f4cb4a99dc4fd689b6184a24e3c9b04069e879e2
Ravi <ravi.rajpurohit385@gmail.com>
2021-07-21 20:28:20 +0530

    updated setup script

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -1,42 +1,8 @@
-git init --bare $HOME/.dotfiles
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-dotfiles config --local status.showUntrackedFiles no
-echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.bashrc
-
+#!/bin/bash
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
-echo ".dotfiles" >> .gitignore
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
-git clone --bare <git-repo-url> $HOME/.dotfiles
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
-mkdir -p .dotfiles-backup && \
-dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
-xargs -I{} mv {} .dotfiles-backup/{}
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-dotfiles checkout
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
-dotfiles config --local status.showUntrackedFiles no
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
-git clone --bare git@github.com:Ravi-Rajpurohit/dotfiles.git $HOME/.dotfiles
+git clone --bare https://github.com/Ravi-Rajpurohit/dotfiles.git $HOME/.dotfiles
 function dotfiles {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
